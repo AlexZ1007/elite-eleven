7152a92ec6c4b4bef88809e892caa81614b72f33
AlexZ1007 <zamfiralexandru1007@gmail.com>
2024-03-19 10:38:21 +0200

    Removed unused functions

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <windows.h>
 #include <fstream>
-#include <Helper.h>
 #include <random>
 
 void SetConsoleColor(WORD color) {
@@ -25,26 +24,15 @@ public:
         return name;
     }
 
-    void setName(const std::string &name) {
-        Player::name = name;
-    }
 
     const std::string &getType() const {
         return type;
     }
 
-    void setType(const std::string &type) {
-        Player::type = type;
-    }
 
     int getOvr() const {
         return ovr;
     }
-
-    void setOvr(int ovr) {
-        Player::ovr = ovr;
-    }
-
     friend std::ostream &operator<<(std::ostream &os, const Player &player) {
         SetConsoleColor(FOREGROUND_GREEN); // Set text color to green
         os <<  player.name << " ";
@@ -72,17 +60,11 @@ public:
         return name;
     }
 
-    void setName(const std::string &name) {
-        Pack::name = name;
-    }
 
     int getCost() const {
         return cost;
     }
 
-    void setCost(int cost) {
-        Pack::cost = cost;
-    }
 
     friend std::ostream &operator<<(std::ostream &os, const Pack &pack) {
         os << "Pack: " << pack.name << "(" << pack.cost<<" gold)";
@@ -167,10 +149,6 @@ public:
 
         }
         printInFile.close();
-    }
-
-    void setName(const std::string &name) {
-        User::name = name;
     }
 
     const std::vector<Player> &getInventory() const {
@@ -274,59 +252,3 @@ int main(){
 
     return 0;
 }
-
-
-
-
-//int main() {
-//    std::cout << "Hello, world!\n";
-//    std::array<int, 100> v{};
-//    int nr;
-//    std::cout << "Introduceti nr: ";
-//    /////////////////////////////////////////////////////////////////////////
-//    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-//    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-//    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-//    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-//    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-//    /// pentru cât mai multe ramuri de execuție.
-//    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-//    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-//    ///
-//    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-//    /// pentru a simula date introduse de la tastatură.
-//    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-//    ///
-//    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-//    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-//    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-//    /// program care merg (și să le evitați pe cele care nu merg).
-//    ///
-//    /////////////////////////////////////////////////////////////////////////
-//    std::cin >> nr;
-//    /////////////////////////////////////////////////////////////////////////
-//    for(int i = 0; i < nr; ++i) {
-//        std::cout << "v[" << i << "] = ";
-//        std::cin >> v[i];
-//    }
-//    std::cout << "\n\n";
-//    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-//    for(int i = 0; i < nr; ++i) {
-//        std::cout << "- " << v[i] << "\n";
-//    }
-//    ///////////////////////////////////////////////////////////////////////////
-//    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-//    /// alt fișier propriu cu ce alt nume doriți.
-//    /// Exemplu:
-//    /// std::ifstream fis("date.txt");
-//    /// for(int i = 0; i < nr2; ++i)
-//    ///     fis >> v2[i];
-//    ///
-//    ///////////////////////////////////////////////////////////////////////////
-//    ///                Exemplu de utilizare cod generat                     ///
-//    ///////////////////////////////////////////////////////////////////////////
-//    Helper helper;
-//    helper.help();
-//    ///////////////////////////////////////////////////////////////////////////
-//    return 0;
-//}
